17cbcd486d7dbeae9f4a740d7b38df12b01ecd6d
Theo Shinigami <theo.cubero01102448@sesisenaipr.org.br>
2026-06-02 11:53:36 -0300

    Catálogo-de-filmes

--- catálogo de filmes/script/script.js
+++ catálogo de filmes/script/script.js
@@ -1,51 +1,77 @@
-const filmes =[
-    {
-        titulo: "taxi drive",
-        Imagem: "https://media.themoviedb.org/t/p/w300_and_h450_face/hvJvOYRrF9xxvNzg2Jd9PYWu7c3.jpg",
-        descrição: "drive",
-    },
-      {
-        titulo: "diverti-damente",
-        Imagem: "https://media.themoviedb.org/t/p/w300_and_h450_face/4Kg34KTlnoHvjLzD7ji2jUSouvD.jpg",
-        descrição: "mente",
-    },
-      {
-        titulo: "psicopata americano",
-        Imagem: "https://media.themoviedb.org/t/p/w300_and_h450_face/rxT7rVM4oLODCzdkkMPWGPMTSt3.jpg",
-        descrição: "americano",
-    },
-     {
-        titulo: "lucas neto em: acampamento de ferias 2",
-        Imagem: "https://media.themoviedb.org/t/p/w300_and_h450_face/vYgZa09ICwAB2TkonMfXRT4Dj8E.jpg",
-        descrição: "acampamento",
-    },
-     {
-        titulo: "miudos e graudos",
-        Imagem: "https://media.themoviedb.org/t/p/w300_and_h450_face/dbU5QOnTCPo4BKZskarx1bBG9lG.jpg",
-        descrição: "graudos",
-    },
-     {
-        titulo: "kung fu futebol clube",
-        Imagem: "https://media.themoviedb.org/t/p/w300_and_h450_face/rO1YoR49hs9QPla0vwCxrMLln33.jpg",
-        descrição: "kung",
-    },
-
+const filmes = [
+    { titulo: "Vingadores", imagem: "https://media.themoviedb.org/t/p/w300_and_h450_face/A4kvp7vY1BDLrrQIagRCffLKj1t.jpg", descricao: "A trama se espalha pelo espaço e pela Terra, colocando os heróis (incluindo os Vingadores, Guardiões da Galáxia, Doutor Estranho e o exército de Wakanda) em batalhas simultâneas para proteger as joias. Apesar de seus esforços heroicos e sacrifícios pessoais, Thanos consegue reunir todas as joias na Manopla do Infinito e atinge seu objetivo com um estalar de dedos, trazendo consequências devastadoras" , breve: "Guerra Infinita" },
+    { titulo: "Backrooms", imagem: "https://media.themoviedb.org/t/p/w300_and_h450_face/a1pgQeEge47xrS4jGlDwIHiieVK.jpg", descricao: "uma adaptação de terror psicológico e ficção científica baseada na famosa lenda urbana da internet creepypasta sobre uma dimensão labiríntica e desolada." ,breve: "Um Não-lugar" },
+    { titulo: "Minecraft", imagem: "https://media.themoviedb.org/t/p/w300_and_h450_face/a227F8PnXDABR1JJTuasmJmxEhf.jpg", descricao: "A história acompanha quatro desajustados (interpretados por Jason Momoa, Sebastian Eugene Hansen, Emma Myers e Danielle Brooks) que são transportados para o Overworld (Mundo Superior). Nesse bizarro país das maravilhas cúbico, eles precisam aprender a sobreviver, usar a imaginação e embarcar em uma jornada mágica com o construtor Steve (Jack Black) para conseguir voltar para casa." , breve: "Um filme" },
+    { titulo: "Motoqueira Fantasma", imagem: "https://media.themoviedb.org/t/p/w300_and_h450_face/vVGVlwRrvVgLPE98rwo8jo3QC2b.jpg", descricao: "é um famoso super-herói/anti-herói sobrenatural da Marvel Comics que ganhou adaptações marcantes no cinema, sendo a mais conhecida a franquia estrelada por Nicolas Cage" , breve: "Ghost rider" },
+    { titulo: "Todo mundo em Panico", imagem: "https://media.themoviedb.org/t/p/w300_and_h450_face/1z9haN6yiYsuv9D93H8n5yaCk8I.jpg", descricao: "A premissa central de quase todos os filmes envolve uma heroína e seus amigos se deparando com situações clássicas de grandes sucessos do cinema (como Pânico, Eu Sei o que Vocês Fizeram no Verão Passado e O Chamado), mas quebrando todas as regras do medo com piadas ácidas, referências à cultura pop e muito non-sense.", breve: "Scary movie" },
+    { titulo: "Gente Grande", imagem: "https://media.themoviedb.org/t/p/w300_and_h450_face/ppU2xJnlKdW3F01AtC9wMuXRZCg.jpg", descricao: "é uma famosa comédia americana estrelada por Adam Sandler, Kevin James, Chris Rock, David Spade e Rob Schneider" , breve: "Grown ups" },
 ]
 
-const listarfilmes = document getElementByid("listarfilmes")
-function mostrafilme(lista){
-    listarfilmes.innerHTML = ""
-    lista.array.forEach(filme => {
-        listarfilmes.innerHTML +=
+const listarFilmes = document.getElementById("listarFilmes");
+
+function mostrarFilmes(lista) {
+    listarFilmes.innerHTML = ""
+    lista.forEach(filme => {
+        listarFilmes.innerHTML += `
         <div class="col-md-4">
-            <div class="card card-filme h-100">
-                <img src="${filme.imagem}"
-                class="card-img-top">
-                    <div class="card-body">
-                        <h3>${filme.titulo}</h3>
-                        <p>${filme.descrição}</p>
-                        <button class="btn btn-dark"
-                        onclick="verDetalhes('${filme.titulo}')">ver detalhes"</button>
+          <div class="card card-filme h-100">
+            <img src="${filme.imagem}" class="card-img-top">
+            <div class="card-body">
+              <h3>${filme.titulo}</h3>
+              <p>${filme.breve}</p>
+              <button class="btn btn-dark" onclick="verDetalhes('${filme.titulo}', '${filme.descricao}')">Ver detalhes</button>
             </div>
-                </div>
-                     </div>
+          </div>
+        </div>
+        `
+    })
+}
+
+mostrarFilmes(filmes)
+
+function verDetalhes(titulo, descricao) {
+    Swal.fire({
+        title: titulo,
+        text: descricao,
+        icon: "info"
+    })
+}
+
+const serie = [
+    { titulo: "Vingadores", imagem: "https://media.themoviedb.org/t/p/w300_and_h450_face/A4kvp7vY1BDLrrQIagRCffLKj1t.jpg", descricao: "A trama se espalha pelo espaço e pela Terra, colocando os heróis (incluindo os Vingadores, Guardiões da Galáxia, Doutor Estranho e o exército de Wakanda) em batalhas simultâneas para proteger as joias. Apesar de seus esforços heroicos e sacrifícios pessoais, Thanos consegue reunir todas as joias na Manopla do Infinito e atinge seu objetivo com um estalar de dedos, trazendo consequências devastadoras" , breve: "Guerra Infinita" },
+    { titulo: "Backrooms", imagem: "https://media.themoviedb.org/t/p/w300_and_h450_face/a1pgQeEge47xrS4jGlDwIHiieVK.jpg", descricao: "uma adaptação de terror psicológico e ficção científica baseada na famosa lenda urbana da internet creepypasta sobre uma dimensão labiríntica e desolada." ,breve: "Um Não-lugar" },
+    { titulo: "Minecraft", imagem: "https://media.themoviedb.org/t/p/w300_and_h450_face/a227F8PnXDABR1JJTuasmJmxEhf.jpg", descricao: "A história acompanha quatro desajustados (interpretados por Jason Momoa, Sebastian Eugene Hansen, Emma Myers e Danielle Brooks) que são transportados para o Overworld (Mundo Superior). Nesse bizarro país das maravilhas cúbico, eles precisam aprender a sobreviver, usar a imaginação e embarcar em uma jornada mágica com o construtor Steve (Jack Black) para conseguir voltar para casa." , breve: "Um filme" },
+    { titulo: "Motoqueira Fantasma", imagem: "https://media.themoviedb.org/t/p/w300_and_h450_face/vVGVlwRrvVgLPE98rwo8jo3QC2b.jpg", descricao: "é um famoso super-herói/anti-herói sobrenatural da Marvel Comics que ganhou adaptações marcantes no cinema, sendo a mais conhecida a franquia estrelada por Nicolas Cage" , breve: "Ghost rider" },
+    { titulo: "Todo mundo em Panico", imagem: "https://media.themoviedb.org/t/p/w300_and_h450_face/1z9haN6yiYsuv9D93H8n5yaCk8I.jpg", descricao: "A premissa central de quase todos os filmes envolve uma heroína e seus amigos se deparando com situações clássicas de grandes sucessos do cinema (como Pânico, Eu Sei o que Vocês Fizeram no Verão Passado e O Chamado), mas quebrando todas as regras do medo com piadas ácidas, referências à cultura pop e muito non-sense.", breve: "Scary movie" },
+    { titulo: "Gente Grande", imagem: "https://media.themoviedb.org/t/p/w300_and_h450_face/ppU2xJnlKdW3F01AtC9wMuXRZCg.jpg", descricao: "é uma famosa comédia americana estrelada por Adam Sandler, Kevin James, Chris Rock, David Spade e Rob Schneider" , breve: "Grown ups" },
+]
+
+const listarSeries = document.getElementById("listarSeries");
+
+function mostrarSerie(lista) {
+    listarSeries.innerHTML = ""
+    lista.forEach(serie => {
+        listarSeries.innerHTML += `
+        <div class="col-md-4">
+          <div class="card card-filme h-100">
+            <img src="${serie.imagem}" class="card-img-top">
+            <div class="card-body">
+              <h3>${serie.titulo}</h3>
+              <p>${serie.breve}</p>
+              <button class="btn btn-dark" onclick="verDetalhes('${serie.titulo}', '${serie .descricao}')">Ver detalhes</button>
+            </div>
+          </div>
+        </div>
+        `
+    })
+}
+
+mostrarSerie(serie)
+
+function verDetalhes(titulo, descricao) {
+    Swal.fire({
+        title: titulo,
+        text: descricao,
+        icon: "info"
+    })
+}
